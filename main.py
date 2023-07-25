d2471702ac1b144088258e8043ae16bcce0d4818
Salman Pey <salman@gmail.dk>
2023-07-25 13:57:31 +0200

    old main

--- main.py
+++ main.py
@@ -79,22 +79,8 @@ def main():
     # Move the file uploader to the sidebar
     uploaded_files = st.file_uploader("Choose IFC files", type='ifc', accept_multiple_files=True, key="file_uploader")
 
-    # Create a sidebar for the search and group options
-    st.sidebar.header("Options")
-
-    # Add a text input for the search term in the sidebar
-    search_term = st.sidebar.text_input("Enter a search term", key="search_term")
-
-    # Add a select box for the group option in the sidebar
-    group_option = st.sidebar.selectbox("Group by", options=["None", "GlobalId", "Name", "Type", "FileName"], key="group_option")
-
-    # SQL Server Parameters
-    st.sidebar.header("SQL Server Parameters")
-    username = st.sidebar.text_input("Username", key="username")
-    password = st.sidebar.text_input("Password", key="password", type="password")
-    server = st.sidebar.text_input("Server address", key="server")
-    database = st.sidebar.text_input("Database name", key="database")
-    table_name = st.sidebar.text_input("Table name", key="table_name")
+    # Create a dictionary to store each DataFrame
+    dfs = {}
 
     for uploaded_file in uploaded_files:
         # Create a temporary file
@@ -103,29 +89,69 @@ def main():
 
         # Convert IFC file to DataFrame and store it in the dictionary
         df = ifc_to_dataframe(tfile.name, uploaded_file.name)  # Pass the file name to ifc_to_dataframe
+        dfs[uploaded_file.name] = df
 
-        # Filter the DataFrame based on the search term
-        if search_term:
-            df = df[df.apply(lambda row: search_term.lower() in row.to_string().lower(), axis=1)]
+    # Create a sidebar for the search and group options
+    st.sidebar.header("Options")
 
-        # Group the DataFrame based on the group option
-        if group_option != "None" and group_option in df.columns:
-            df = df.groupby(group_option).first().reset_index()
+    # Add a checkbox for the user to select each model
+    model_options = {model: st.checkbox("Select " + model, value=True) for model in dfs.keys()}
+
+    # Add a text input for the search term in the sidebar
+    search_term = st.sidebar.text_input("Enter a search term", key="search_term")
+
+    # Add a select box for the group option in the sidebar
+    group_option = st.sidebar.selectbox("Group by", options=["None"] + list(set(col for df in dfs.values() for col in df.columns)), key="group_option")
+
+    # Create a list to store the dataframes of the selected models
+    selected_dfs = []
+
+    # For each selected model, add its DataFrame to the list
+    for model_option, is_selected in model_options.items():
+        if is_selected:
+            # Get the DataFrame for the chosen model
+            df = dfs[model_option]
+
+            # Filter the DataFrame based on the search term
+            if search_term:
+                df = df[df.apply(lambda row: search_term.lower() in row.to_string().lower(), axis=1)]
+
+            # Group the DataFrame based on the group option
+            if group_option != "None" and group_option in df.columns:
+                df = df.groupby(group_option).first().reset_index()
+
+            # Add the DataFrame to the list
+            selected_dfs.append(df)
+
+    # Concatenate all the selected dataframes into a single dataframe
+    if selected_dfs:
+        all_df = pd.concat(selected_dfs, ignore_index=True)
 
         # Display the DataFrame
-        st.dataframe(df)
-
-        # Export to Excel button
-        if st.sidebar.button('Export to Excel ' + uploaded_file.name):
-            st.sidebar.markdown(get_excel_download_link(df, filename=f"{uploaded_file.name}.xlsx"), unsafe_allow_html=True)
-
-        # Export to SQL Server button
-        if st.sidebar.button('Export to SQL Server ' + uploaded_file.name) and server and username and password and database and table_name:
+        st.dataframe(all_df)
+        
+          # Export to Excel button
+        if st.sidebar.button('Export to Excel'):
+            st.sidebar.markdown(get_excel_download_link(all_df), unsafe_allow_html=True)
+            
+           # Export to SQL Server button
+        if st.sidebar.button('Export to SQL Server') and server and username and password and database and table_name:
             connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
             engine = create_engine(connection_string)
-            df.to_sql(table_name, engine, if_exists='append')  # append to existing table if it exists
-            st.sidebar.write(f'Data from {uploaded_file.name} written to SQL Server')
+            all_df.to_sql(table_name, engine, if_exists='replace')  # replace existing table if it exists
+            st.sidebar.write('Data written to SQL Server')
 
+      
+
+        # SQL Server Parameters
+        st.sidebar.header("SQL Server Parameters")
+        username = st.sidebar.text_input("Username", key="username")
+        password = st.sidebar.text_input("Password", key="password", type="password")
+        server = st.sidebar.text_input("Server address", key="server")
+        database = st.sidebar.text_input("Database name", key="database")
+        table_name = st.sidebar.text_input("Table name", key="table_name")
+
+     
 
 if __name__ == "__main__":
     main()
