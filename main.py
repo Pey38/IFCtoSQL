185b631ffc194c14ba2b15df85704858d9dd5abe
Salman Pey <salman@gmail.dk>
2023-07-25 14:44:06 +0200

    dataframe removed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import ifcopenshell
 import tempfile
 from sqlalchemy import create_engine
@@ -8,17 +7,16 @@ import io
 import os
 
 
-
-def ifc_to_dataframe(ifc_file_path, file_name):
+def ifc_to_list(ifc_file_path, file_name):
     """
-    Convert an IFC file to a DataFrame.
+    Convert an IFC file to a list of dictionaries.
 
     Args:
         ifc_file_path (str): The path to the IFC file.
         file_name (str): The name of the IFC file.
 
     Returns:
-        pd.DataFrame: The DataFrame containing the IFC data.
+        list: The list containing dictionaries with the IFC data.
     """
 
     # Use ifcopenshell or similar library to read the IFC file
@@ -51,27 +49,28 @@ def ifc_to_dataframe(ifc_file_path, file_name):
 
         data.append(entity_data)
 
-    # Convert data to DataFrame
-    df = pd.DataFrame(data)
-
-    # Delete rows where Type equals 'IfcGroup' and 'IfcOpeningElement'
-    df = df[df.Type != 'IfcGroup']
-    df = df[df.Type != 'IfcOpeningElement']
-
-    return df
+    return data
 
 
-def get_excel_download_link(df, filename="data.xlsx"):
+def get_excel_download_link(data_list, filename="data.xlsx"):
     """
-    Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
+    Generates a link allowing the data in a given list of dictionaries to be downloaded in an Excel file.
+
+    Args:
+        data_list (list): The list of dictionaries containing the data.
+        filename (str): The name of the Excel file to be downloaded.
+
+    Returns:
+        str: The HTML link for downloading the Excel file.
     """
+    df = pd.DataFrame(data_list)
     excel_file = io.BytesIO()
     df.to_excel(excel_file, index=False)
     excel_file.seek(0)
     b64 = base64.b64encode(excel_file.read()).decode()  # some strings
     return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">Download excel file</a>'
+
+
 def main():
     st.title('IFC to SQL Server and Excel Converter')
 
@@ -86,8 +85,8 @@ def main():
     # File Uploader
     uploaded_files = st.file_uploader("Choose IFC files", type='ifc', accept_multiple_files=True)
 
-    # Initialize an empty DataFrame to gather data from all files
-    all_data_dfs = []
+    # Initialize an empty list to gather data from all files
+    all_data_list = []
 
     if uploaded_files:
         for uploaded_file in uploaded_files:
@@ -95,41 +94,47 @@ def main():
             tfile = tempfile.NamedTemporaryFile(delete=False)
             tfile.write(uploaded_file.getvalue())
 
-            # Convert IFC file to DataFrame
-            df = ifc_to_dataframe(tfile.name, uploaded_file.name)
+            # Convert IFC file to a list of dictionaries
+            data_list = ifc_to_list(tfile.name, uploaded_file.name)
 
-            # Append data from this file to the main DataFrame
-            all_data_dfs.append(df)
+            # Append data from this file to the main list
+            all_data_list.extend(data_list)
 
-    if all_data_dfs:
-        # Concatenate all DataFrames into a single DataFrame
-        all_data_df = pd.concat(all_data_dfs, ignore_index=True)
-
-        # Filter the DataFrame based on the search term and group option
+    if all_data_list:
+        # Filter the data based on the search term and group option
         search_term = st.sidebar.text_input("Enter a search term")
-        group_option = st.sidebar.selectbox("Group by", options=["None"] + list(all_data_df.columns))
+        group_option = st.sidebar.selectbox("Group by", options=["None", "GlobalId", "Name", "Type"])
 
         if search_term:
-            all_data_df = all_data_df[all_data_df.apply(lambda row: search_term.lower() in row.to_string().lower(), axis=1)]
+            all_data_list = [item for item in all_data_list if search_term.lower() in str(item).lower()]
 
-        if group_option != "None" and group_option in all_data_df.columns:
-            all_data_df = all_data_df.groupby(group_option).first().reset_index()
+        if group_option != "None" and group_option in all_data_list[0]:
+            # Group the data based on the selected column
+            grouped_data = {}
+            for item in all_data_list:
+                key = item[group_option]
+                if key not in grouped_data:
+                    grouped_data[key] = item
+            all_data_list = list(grouped_data.values())
 
-        # Display the DataFrame
-        st.dataframe(all_data_df)
+        # Display the data
+        st.write(all_data_list)
 
         # Export to Excel
         if st.sidebar.button('Export to Excel'):
-            st.sidebar.markdown(get_excel_download_link(all_data_df), unsafe_allow_html=True)
+            st.sidebar.markdown(get_excel_download_link(all_data_list), unsafe_allow_html=True)
 
         # Export to SQL Server
         if st.sidebar.button('Export to SQL Server') and server and username and password and database and table_name:
             connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
             engine = create_engine(connection_string)
-            all_data_df.to_sql(table_name, engine, if_exists='append')  # append to existing table if it exists
+            with engine.connect() as conn:
+                for item in all_data_list:
+                    conn.execute(f"INSERT INTO {table_name} VALUES {tuple(item.values())}")
             st.sidebar.write('Data written to SQL Server')
     else:
         st.write("No data found. Please upload IFC files.")
+
 
 if __name__ == "__main__":
     main()
